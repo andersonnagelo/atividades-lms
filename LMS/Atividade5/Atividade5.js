d51bfb270b664309176a379b69e5bb2442e32af3
andersonnagelo <31452157+andersonnagelo@users.noreply.github.com>
2017-10-17 23:26:12 -0300

    Editando

--- LMS/Atividade5/Atividade5.js
+++ LMS/Atividade5/Atividade5.js
@@ -1,37 +1,34 @@
 let menuSanduiche = document.querySelector(".menu-sanduiche");
 let menuLateral = document.querySelector(".menu-lateral");
-let itens = document.querySelector(".itens");
-let acordeon = document.querySelector(".acordeon");
-let menuAcordeon = document.querySelector(".menu-acordeon")
 
-/*function abrirFechar() {
-    menuSanduiche.addEventListener("click", function () {
-        menuLateralclassList.toggle("ativo");
+menuSanduiche.addEventListener("click", function () {
+    menuLateral.classList.toggle("ativo");
+    menuAcordeon.classList.toggle("ativo");
 
-    });*/
+});
 
 /*function abrirMenu() {
     menuAcordeon.style.transform = "translateX(200px)"
     menuLateral.style.transform = "translateX(0px)";
 };*/
 
+let itensAcordeon = document.querySelectorAll(".itens");
+let conteudoAcordeon = document.querySelectorAll(".acordeon ul .conteudo");
 
-function abrirMenu() {
-    menuAcordeon.style.transform = "translateX(200px)"
-    menuLateral.style.transform = "translateX(0px)";
-};
+for (let i = 0; i < itensAcordeon.length; i++) {
+    itensAcordeon[i].addEventListener("click", function () {
+        if (conteudoAcordeon[i].classList.contains("ativo")) {
+            conteudoAcordeon[i].classList.remove("ativo");
 
-menuSanduiche.addEventListener('click', function () {
-    abrirMenu();
-});
+            conteudoAcordeon[i].style.maxHeight = "0px";
+        } else {
+            conteudoAcordeon.forEach(function (e) {
+                e.classList.remove("ativo");
+                e.style.maxHeight = "0px";
+            });
+            conteudoAcordeon[i].classList.add("ativo");
+            conteudoAcordeon[i].style.maxHeight = conteudoAcordeon[i].scrollHeight + "px";
+        }
 
-function conteudoAcordeon() {
-    if (itens.style.animationName == "abrir-acordeon") {
-        itens.style.animationName = "fechar-acordeon"
-    } else {
-        itens.style.animationName = "abrir-acordeon"
-    }
-
+    });
 }
-
-itens.addEventListener("click", conteudoAcordeon);
